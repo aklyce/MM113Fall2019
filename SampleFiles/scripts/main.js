46b30ec54c2f44b1001bb19ae5fbc03a39b20a5e
aklyce <aklyce@berkeley.edu>
2020-03-30 20:55:06 -0700

    Final Assignment_4

--- SampleFiles/scripts/main.js
+++ SampleFiles/scripts/main.js
@@ -11,13 +11,18 @@ const modifyText = () => {
   //Change the text of user2 to be the handle from the StarTrekTweet object
   //Changes the text of message 2 class to be the handle from the StarTrekTweet object
   // YOUR CODE BELOW THIS LINE.
+    document.getElementById("message2").innerHTML = StarTrekTweet.text;
+    document.getElementById("user2").innerHTML = StarTrekTweet.name;
+    document.querySelector('#link2').innerHTML = StarTrekTweet.handle;
 }
 
 const changeLinkAndImageAttributes = () => {
   //Set the href attribute for link1 to point to the first element in the links array
   //Sets the href attribute for link2 to point to the second element in the links array
   //Sets the src attribute for the Ohlone image to point to the third element in the links array
-  // YOUR CODE BELOW THIS LINE.
+    document.querySelector('#link1').href = links[0];
+    document.querySelector('#link2').href = links[1];
+    document.querySelector('#Ohlone').src = links[2];
 }
 
 const changeClassAttribute = () => {
@@ -25,6 +30,7 @@ const changeClassAttribute = () => {
   //function so that it replaces the “tweet” class with the “tweet-pink” class for the two tweets.
   // change id="tweet1" class="tweet" to id="tweet1" class="tweet-pink"
   // YOUR CODE BELOW THIS LINE
+    document.getElementById('tweet1').className = 'tweet-pink';
   
 }
 
@@ -35,5 +41,11 @@ const appendElements = () => {
   //points to an image of your choice,symbolizing something about the course.
   //id="tweet1"
   // YOUR CODE BELOW THIS LINE
-
+    const newLink = document.createElement ('a');
+    newLink.href = 'http://www.ohlone.edu';
+    newLink.appendChild(document.createTextNode('Intro to Javascript'));
+    document.getElementById('tweet1').appendChild(newLink);
+    const img = document.createElement('img');
+    img.src = "https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg"
+    document.getElementById('tweet1').appendChild(img);
 }
